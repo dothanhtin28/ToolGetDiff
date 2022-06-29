6347a1b5899343d7482b0ff08975c52ae9f9b1d2
Threat <dothanhtin22@hotmail.com>
2022-06-29 22:55:22 +0700

    First commit

---
+++ ComputeLoadFileConvert2ListStrring/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ComputeLoadFileConvert2ListStrring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var swiftFilePath = @"E:\Work\SampleCodes\ComputeLoadFileConvert2ListStrring\docs\filePathDataSwift.txt";
+            List<string> dataOfSwift = System.IO.File.ReadLines(swiftFilePath).ToList();
+            var dbFilePath = @"E:\Work\SampleCodes\ComputeLoadFileConvert2ListStrring\docs\filePathDataCouchbase.txt";
+            List<string> dataOfDb = System.IO.File.ReadLines(dbFilePath).ToList();
+
+            var diffFilePaths = dataOfSwift.Except(dataOfDb);
+            //var diffFilePaths = dataOfDb.Except(dataOfSwift);
+            File.WriteAllLines(@"E:\Work\SampleCodes\ComputeLoadFileConvert2ListStrring\docs\Compare.txt", diffFilePaths);
+        }
+    }
+}
